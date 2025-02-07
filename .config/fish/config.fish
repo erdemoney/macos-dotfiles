f6376800a28613356e3921471446e204218893d9
Eric <edemoney@ciena.com>
2025-02-06 23:19:33 -0800

    add fish configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+# Commands to run in interactive sessions can go here
+if status is-interactive
+    fish_vi_key_bindings
+    atuin init fish | source
+end
+
+# Created by `pipx` on 2025-01-12 19:54:46
+set PATH $PATH /Users/edemoney/.local/bin
